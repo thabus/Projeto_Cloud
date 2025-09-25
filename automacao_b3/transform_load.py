08198fb1e0dd15df8e86390550f13da4e2768a32
emi-fernandes <emillylafernandes@gmail.com>
2025-09-24 21:28:37 -0300

    transform_load: baixa XML do blob e prepara BytesIO

--- automacao_b3/transform_load.py
+++ automacao_b3/transform_load.py
@@ -1,10 +1,22 @@
+import io
 from azure_storage import get_file_from_blob
+from lxml import etree
 
 DATA_FILE = "250923"
 FILE_NAME = f"BVBG186_{DATA_FILE}.xml"
 
 def transform_and_load():
     xml_content = get_file_from_blob(FILE_NAME)
-    print(xml_content[:500])  # Exibe os primeiros 500 caracteres do conteúdo do XML
+    xml_bytes = io.BytesIO(xml_content.encode('utf-8'))
 
-transform_and_load()
+#BUSCAR TckrSymb (nome das Ações)
+#BUSCAR TradDtls (Detalhe das Negociações)
+#volume financeiro,
+#preço maximo,
+#preço minimo,
+#preço fechamento,
+#preço abertura,
+#data da negociação
+
+    for _, elemXml in etree.ioterparse(xml_bytes, tag="{urn:bvmf.217.01.xsd}TckrSymb", huge_tree=True ):
+        print(f"Ação: {elemXml.text} ")
